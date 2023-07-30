c0e2de999b22c5eccf6808ac12ae54cd9415af48
cpesar <cpesar1@gmail.com>
2023-07-30 16:20:46 -0600

    added context api to handle global state and added react icons

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
+
+import * as dotenv from 'dotenv';
+dotenv.config()
 import express from 'express';
 const app = express();
+import morgan from 'morgan';
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+const port = process.env.PORT || 5100
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Does this really work');
 });
 
-app.listen(5100, () => {
-    console.log('server running....');
+app.post('/', (req, res) => {
+    console.log(req)
+    res.json({ message: 'Data received', data: req.body })
+})
+
+// 
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
 });
