75e8ecae7f2daf58ce0a3436da0368c916fc885d
cpesar <cpesar1@gmail.com>
2023-07-30 20:12:27 -0600

    added nanoid dependency for creating unique ids

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ import express from 'express';
 const app = express();
 import morgan from 'morgan';
 
+// const getData = async () => {
+//     const response = await fetch('https://www.course-api.com/react-useReducer-cart-project')
+//     const cartData = await response.json()
+//     console.log(cartData)
+// }
+// getData()
+
+try {
+    const response = await fetch('https://www.course-api.com/react-useReducer-cart-project')
+    const cartData = await response.json()
+    console.log(cartData)
+} catch (err) {
+    console.log(err)
+}
+
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
@@ -13,7 +28,7 @@ const port = process.env.PORT || 5100
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Does this really work');
+    res.send('Hello user');
 });
 
 app.post('/', (req, res) => {
