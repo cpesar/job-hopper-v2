5851fc75f7e3747b3e836f8e9fefd927f4966925
cpesar <cpesar1@gmail.com>
2023-07-30 21:28:06 -0600

    tested CRUD routes with nanoid

--- server.js
+++ server.js
@@ -4,21 +4,12 @@ dotenv.config()
 import express from 'express';
 const app = express();
 import morgan from 'morgan';
+import { nanoid } from 'nanoid';
 
-// const getData = async () => {
-//     const response = await fetch('https://www.course-api.com/react-useReducer-cart-project')
-//     const cartData = await response.json()
-//     console.log(cartData)
-// }
-// getData()
-
-try {
-    const response = await fetch('https://www.course-api.com/react-useReducer-cart-project')
-    const cartData = await response.json()
-    console.log(cartData)
-} catch (err) {
-    console.log(err)
-}
+let jobs = [
+    { id: nanoid(), company: 'apple', position: 'front-end' },
+    { id: nanoid(), company: 'google', position: 'back-end' },
+];
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
@@ -36,7 +27,64 @@ app.post('/', (req, res) => {
     res.json({ message: 'Data received', data: req.body })
 })
 
-// 
+// GET ALL JOBS
+app.get('/api/v1/jobs', (req, res) => {
+    res.status(200).json({ jobs });
+});
+
+// CREATE A JOB
+app.post('/api/v1/jobs', (req, res) => {
+    const { company, position } = req.body
+    if (!company || !position) {
+        return res.status(400).json({ message: 'Please provide company and position' })
+    }
+    const id = nanoid(10)
+
+    const job = { id, company, position }
+    jobs.push(job)
+    res.status(200).json({ job })
+});
+
+//GET A SINGLE JOB
+app.get('/api/v1/jobs/:id', (req, res) => {
+    const { id } = req.params;
+    const job = jobs.find((job) => job.id === id)
+    if (!job) {
+        return res.status(400).json({ message: `No job with id, ${id}` })
+    }
+    res.status(200).json({ job })
+});
+
+//EDIT A JOB
+app.patch('/api/v1/jobs/:id', (req, res) => {
+    const { company, position } = req.body;
+    if (!company || !position) {
+        return res.status(400).json({ msg: 'please provide company and position' });
+    }
+    const { id } = req.params;
+    const job = jobs.find((job) => job.id === id);
+    if (!job) {
+        return res.status(404).json({ msg: `no job with id ${id}` });
+    }
+
+    job.company = company;
+    job.position = position;
+    res.status(200).json({ msg: 'job modified', job });
+});
+
+//DELETE A JOB
+app.delete('/api/v1/jobs/:id', (req, res) => {
+    const { id } = req.params
+    const job = jobs.find((job) => job.id === id);
+    if (!job) {
+        return res.status(400).json({ message: 'Job with id ${id}' })
+    }
+    const newJobs = jobs.filter((job) => job.id !== id)
+    jobs = newJobs
+
+    res.status(200).json({ message: 'Job deleted' })
+})
+
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 });
