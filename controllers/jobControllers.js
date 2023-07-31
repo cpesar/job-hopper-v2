26bf535856ff1ae15b7bcfc3e768d4187df6b120
cpesar <cpesar1@gmail.com>
2023-07-31 10:58:37 -0600

    custom middlware for error handling

--- controllers/jobControllers.js
+++ controllers/jobControllers.js
@@ -1,63 +1,44 @@
-import { nanoid } from 'nanoid';
 import Job from '../models/JobModel.js';
+import { StatusCodes } from 'http-status-codes';
+import { NotFoundError } from '../errors/customErrors.js';
 
-let jobs = [
-    { id: nanoid(), company: 'apple', position: 'front-end' },
-    { id: nanoid(), company: 'google', position: 'back-end' },
-];
 
 export const getAllJobs = async (req, res) => {
     const jobs = await Job.find({})
-    res.status(200).json({ jobs });
+    res.status(StatusCodes.OK).json({ jobs });
 };
-
-// Don't need to do try catch with express-errors package
-// export const createJob = async (req, res) => {
-//     try {
-//         const job = await Job.create(req.body)
-//         res.status(201).json({ job })
-//     } catch (err) {
-//         console.log(err)
-//         res.status(500).json({ msg: 'server error' })
-//     }
-// };
 
 export const createJob = async (req, res) => {
     const job = await Job.create(req.body);
-    res.status(201).json({ job });
+    res.status(StatusCodes.CREATED).json({ job });
 };
-
 
 export const getSingleJob = async (req, res) => {
     const { id } = req.params;
-    const job = await Job.findById(id);
-    if (!job) {
-        return res.status(400).json({ message: `No job with id, ${id}` })
-    }
-    res.status(200).json({ job })
-};
 
+    const job = await Job.findById(id);
+    if (!job) throw new NotFoundError(`no job with id ${id}`)
+    res.status(StatusCodes.OK).json({ job })
+};
 
 export const updateJob = async (req, res) => {
     const { id } = req.params;
+
     const updatedJob = await Job.findByIdAndUpdate(id, req.body, {
         new: true,
     })
     if (!updatedJob) {
-        return res.status(404).json({ msg: `no job with id ${id}` });
+        return res.status(StatusCodes.NOT_FOUND).json({ msg: `no job with id ${id}` });
     }
-    res.status(200).json({ job: updatedJob });
+    res.status(StatusCodes.OK).json({ job: updatedJob });
 };
-
 
 export const deleteJob = async (req, res) => {
     const { id } = req.params
 
     const removedJob = await Job.findOneAndDelete(id)
     if (!removedJob) {
-        return res.status(400).json({ message: 'Job with id ${id}' })
+        return res.status(StatusCodes.BAD_REQUEST).json({ msg: 'Job with id ${id}' })
     }
-
-
-    res.status(200).json({ message: 'Job deleted', job: removedJob });
+    res.status(StatusCodes.OK).json({ msg: 'Job deleted', job: removedJob });
 }
